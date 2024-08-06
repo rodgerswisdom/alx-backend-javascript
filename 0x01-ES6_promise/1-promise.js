75c8f124be520e52ad4bef26b8e97089aace5427
rodgerswisdom <trulyhawona@gmail.com>
2024-08-06 23:57:10 +0300

    feat: 1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+// 1. Don't make a promise...if you know you can't keep it
+
+export default function getFullResponseFromAPI(success) {
+    return new Promise( (resolve, reject) =>{
+
+        if (success == true) {
+            resolve({
+                status: 200,
+                body: 'Success'
+            })
+        } else {
+            reject('The fake API is not working currently')
+        }
+    } )
+}
