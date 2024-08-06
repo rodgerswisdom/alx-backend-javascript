f830c9536858ef188e529c393deaa60e3ccae775
rodgerswisdom <trulyhawona@gmail.com>
2024-08-07 00:16:41 +0300

    feat: 2-then.js

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -1,4 +1,9 @@
-import getFullResponseFromAPI from './1-promise';
+// import getFullResponseFromAPI from './1-promise';
 
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
+// console.log(getFullResponseFromAPI(true));
+// console.log(getFullResponseFromAPI(false));
+
+import handleResponseFromAPI from "./2-then";
+
+const promise = Promise.resolve();
+handleResponseFromAPI(promise);
